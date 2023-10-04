9f67ca63ddcf129f20299641944d89fd3a6ec7ea
mjbonney <146850633+mjbonney@users.noreply.github.com>
2023-10-04 16:49:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import pandas
 import requests
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,3 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment - what does this do? This line creates a dataframe table from the json values
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
